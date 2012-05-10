feb04491a20007dd9cc09df9110eb1ff6b6b5131
mattn <mattn.jp@gmail.com>
2012-05-10 17:49:06 +0900

    fix example.

--- example/example.go
+++ example/example.go
@@ -4,7 +4,7 @@ import "github.com/mattn/go-mruby"
 
 func main() {
 	mrb := mruby.New()
-	for i := range mrb.Eval(`[1,2,3].map {|x| x + 1}`).([]interface{}) {
-		println(int(i))
+	for _, i := range mrb.Eval(`[1,2,3].map {|x| x + 1}`).([]interface{}) {
+		println(i.(int32)) // 2 3 4
 	}
 }
